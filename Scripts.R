f1837584d22c00ee362d5694030e3290d09ac183
Bill Lin <chengblin@gmail.com>
2025-12-21 12:27:13 -0500

    start analysis

--- Scripts.R
+++ Scripts.R
@@ -13,23 +13,229 @@ df.dem <- read.xlsx("dem-data.xlsx")
 df.cont <- left_join(df.cont, df.dem, by = "author")
 df.bin <- left_join(df.bin, df.dem, by = "author")
 
-df.reintubation <- df.bin %>% filter(outcome == "reintubation")
-df.pneumonia <- df.bin %>% filter(outcome == "pneumonia")
-df.atelectasis <- df.bin %>% filter(outcome == "atelectasis")
-df.hypoxemia <- df.bin %>% filter(outcome == "hypoxemia")
-df.mortality <- df.bin %>% filter(outcome == "mortality")
-df.toniv <- df.bin %>% filter(outcome == "toniv")
-df.ppc <- df.bin %>% filter(outcome == "ppc")
+df.reintubation <- df.bin %>% filter(outcome == "reintubation")%>% filter(control1 != "NIV")
+df.pneumonia <- df.bin %>% filter(outcome == "pneumonia")%>% filter(control1 != "NIV")
+df.atelectasis <- df.bin %>% filter(outcome == "atelectasis")%>% filter(control1 != "NIV")
+df.hypoxemia <- df.bin %>% filter(outcome == "hypoxemia")%>% filter(control1 != "NIV")
+df.mortality <- df.bin %>% filter(outcome == "mortality")%>% filter(control1 != "NIV")
+df.toniv <- df.bin %>% filter(outcome == "toniv")%>% filter(control1 != "NIV")
+df.ppc <- df.bin %>% filter(outcome == "ppc")%>% filter(control1 != "NIV")
 
-df.hlos <- df.cont %>% filter(outcome =="hlos")
-df.iculos <- df.cont %>% filter(outcome =="iculos")
-df.discomfort <- df.cont %>% filter(outcome =="discomfort")
-df.pfr2h <- df.cont %>% filter(outcome =="pfr2h")
-df.paco2early <- df.cont %>% filter(outcome =="paco2early")
-df.pao2early <- df.cont %>% filter(outcome =="pao2early")
-df.phearly <- df.cont %>% filter(outcome =="phearly")
-df.rrearly <- df.cont %>% filter(outcome =="rrearly")
-df.spoearly <- df.cont %>% filter(outcome =="spoearly")
-df.hrearly <- df.cont %>% filter(outcome =="hrearly")
-df.map <- df.cont %>% filter(outcome =="map")
+df.hlos <- df.cont %>% filter(outcome =="hlos")%>% filter(control1 != "NIV")
+df.iculos <- df.cont %>% filter(outcome =="iculos")%>% filter(control1 != "NIV")
+df.discomfort <- df.cont %>% filter(outcome =="discomfort")%>% filter(control1 != "NIV")
+df.pfr2h <- df.cont %>% filter(outcome =="pfr2h")%>% filter(control1 != "NIV")
+df.paco2early <- df.cont %>% filter(outcome =="paco2early")%>% filter(control1 != "NIV")
+df.pao2early <- df.cont %>% filter(outcome =="pao2early")%>% filter(control1 != "NIV")
+df.phearly <- df.cont %>% filter(outcome =="phearly")%>% filter(control1 != "NIV")
+df.rrearly <- df.cont %>% filter(outcome =="rrearly")%>% filter(control1 != "NIV")
+df.spoearly <- df.cont %>% filter(outcome =="spoearly")%>% filter(control1 != "NIV")
+df.hrearly <- df.cont %>% filter(outcome =="hrearly")%>% filter(control1 != "NIV")
+df.map <- df.cont %>% filter(outcome =="map")%>% filter(control1 != "NIV")
 
+#forest 
+
+m.reintu <- metabin(event.e = n.e.outcome, 
+                 n.e = n.e,
+                 event.c = n.c1.outcome,
+                 n.c = n.c1,
+                 studlab = author,
+                 data = df.reintubation,
+                 sm = "OR",
+                 method = "Peto",
+                 MH.exact = TRUE,
+                 common = TRUE,
+                 random = TRUE,
+                 method.tau = "PM",
+                 method.random.ci = "HK",
+                 title = "Odds ratio for re-intubation")
+summary(m.reintu)
+
+m.pneumo <- metabin(event.e = n.e.outcome, 
+                    n.e = n.e,
+                    event.c = n.c1.outcome,
+                    n.c = n.c1,
+                    studlab = author,
+                    data = df.pneumonia,
+                    sm = "OR",
+                    method = "Peto",
+                    MH.exact = TRUE,
+                    common = TRUE,
+                    random = TRUE,
+                    method.tau = "PM",
+                    method.random.ci = "HK",
+                    title = "Odds ratio for pneumonia")
+summary(m.pneumo)
+
+m.ate <- metabin(event.e = n.e.outcome, 
+                    n.e = n.e,
+                    event.c = n.c1.outcome,
+                    n.c = n.c1,
+                    studlab = author,
+                    data = df.atelectasis,
+                    sm = "OR",
+                    method = "Peto",
+                    MH.exact = TRUE,
+                    common = TRUE,
+                    random = TRUE,
+                    method.tau = "PM",
+                    method.random.ci = "HK",
+                    title = "Odds ratio for atelectasis")
+summary(m.ate)
+
+m.hypox <- metabin(event.e = n.e.outcome, 
+                   n.e = n.e,
+                   event.c = n.c1.outcome,
+                   n.c = n.c1,
+                   studlab = author,
+                   data = df.hypoxemia,
+                   sm = "OR",
+                   method = "Peto",
+                   MH.exact = TRUE,
+                   common = TRUE,
+                   random = TRUE,
+                   method.tau = "PM",
+                   method.random.ci = "HK",
+                   title = "Odds ratio for hypoxemia")
+summary(m.hypox)
+
+m.mortal <- metabin(event.e = n.e.outcome, 
+                   n.e = n.e,
+                   event.c = n.c1.outcome,
+                   n.c = n.c1,
+                   studlab = author,
+                   data = df.mortality,
+                   sm = "OR",
+                   method = "Peto",
+                   MH.exact = TRUE,
+                   common = TRUE,
+                   random = TRUE,
+                   method.tau = "PM",
+                   method.random.ci = "HK",
+                   title = "Odds ratio for mortality")
+summary(m.mortal)
+
+m.toniv <- metabin(event.e = n.e.outcome, 
+                    n.e = n.e,
+                    event.c = n.c1.outcome,
+                    n.c = n.c1,
+                    studlab = author,
+                    data = df.toniv,
+                    sm = "OR",
+                    method = "Peto",
+                    MH.exact = TRUE,
+                    common = TRUE,
+                    random = TRUE,
+                    method.tau = "PM",
+                    method.random.ci = "HK",
+                    title = "Odds ratio for escalate to NIV")
+summary(m.toniv)
+m.ppc <- metabin(event.e = n.e.outcome, 
+                   n.e = n.e,
+                   event.c = n.c1.outcome,
+                   n.c = n.c1,
+                   studlab = author,
+                   data = df.ppc,
+                   sm = "OR",
+                   method = "Peto",
+                   MH.exact = TRUE,
+                   common = TRUE,
+                   random = TRUE,
+                   method.tau = "PM",
+                   method.random.ci = "HK",
+                   title = "Odds ratio for PPC")
+summary(m.ppc)
+
+m.hlos <- metacont(n.e = n.e,
+                          mean.e = e.outcome,
+                          sd.e = e.sd,
+                          n.c = n.c1,
+                          mean.c = c1.outcome,
+                          sd.c = c1.sd,
+                          studlab = author,
+                          data = df.hlos,
+                          sm = "MD",
+                          common = TRUE,
+                          random = TRUE,
+                          method.tau = "REML",
+                          method.random.ci = "HK",
+                          title = "Hospital length of stay")
+summary(m.hlos)
+m.iculos <- metacont(n.e = n.e,
+                   mean.e = e.outcome,
+                   sd.e = e.sd,
+                   n.c = n.c1,
+                   mean.c = c1.outcome,
+                   sd.c = c1.sd,
+                   studlab = author,
+                   data = df.iculos,
+                   sm = "MD",
+                   common = TRUE,
+                   random = TRUE,
+                   method.tau = "REML",
+                   method.random.ci = "HK",
+                   title = "ICU length of stay")
+summary(m.iculos)
+
+m.discom <- metacont(n.e = n.e,
+                     mean.e = e.outcome,
+                     sd.e = e.sd,
+                     n.c = n.c1,
+                     mean.c = c1.outcome,
+                     sd.c = c1.sd,
+                     studlab = author,
+                     data = df.discomfort,
+                     sm = "MD",
+                     common = TRUE,
+                     random = TRUE,
+                     method.tau = "REML",
+                     method.random.ci = "HK",
+                     title = "Discomfort")
+summary(m.discom)
+
+m.pfr <- metacont(n.e = n.e,
+                     mean.e = e.outcome,
+                     sd.e = e.sd,
+                     n.c = n.c1,
+                     mean.c = c1.outcome,
+                     sd.c = c1.sd,
+                     studlab = author,
+                     data = df.pfr2h,
+                     sm = "MD",
+                     common = TRUE,
+                     random = TRUE,
+                     method.tau = "REML",
+                     method.random.ci = "HK",
+                     title = "Early PF Ratio")
+summary(m.pfr)
+
+m.pao2 <- metacont(n.e = n.e,
+                  mean.e = e.outcome,
+                  sd.e = e.sd,
+                  n.c = n.c1,
+                  mean.c = c1.outcome,
+                  sd.c = c1.sd,
+                  studlab = author,
+                  data = df.pao2early,
+                  sm = "MD",
+                  common = TRUE,
+                  random = TRUE,
+                  method.tau = "REML",
+                  method.random.ci = "HK",
+                  title = "Early PaO2")
+summary(m.pao2)
+m.paco2 <- metacont(n.e = n.e,
+                   mean.e = e.outcome,
+                   sd.e = e.sd,
+                   n.c = n.c1,
+                   mean.c = c1.outcome,
+                   sd.c = c1.sd,
+                   studlab = author,
+                   data = df.paco2early,
+                   sm = "MD",
+                   common = TRUE,
+                   random = TRUE,
+                   method.tau = "REML",
+                   method.random.ci = "HK",
+                   title = "Early PaO2")
+summary(m.paco2)
