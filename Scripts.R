2c7e636f2245b9f5b42436c385bcf7121fcfbc32
Bill Lin <chengblin@gmail.com>
2025-12-25 12:58:23 -0500

    final analysis

--- Scripts.R
+++ Scripts.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(openxlsx)
 library(metafor)
 library(dbplyr)
+library(RTSA)
 
 df.cont <- read.xlsx("cont-data.xlsx")
 df.bin <- read.xlsx("bin-data.xlsx")
@@ -32,6 +33,20 @@ df.rrearly <- df.cont %>% filter(outcome =="rrearly")%>% filter(control1 != "NIV
 df.spoearly <- df.cont %>% filter(outcome =="spoearly")%>% filter(control1 != "NIV")
 df.hrearly <- df.cont %>% filter(outcome =="hrearly")%>% filter(control1 != "NIV")
 df.map <- df.cont %>% filter(outcome =="map")%>% filter(control1 != "NIV")
+
+df.reintubation <- df.reintubation %>% arrange(author)
+
+df.composite <- inner_join(df.reintubation, df.toniv, 
+                           by = c("author", "year"), 
+                           suffix = c(".reintub", ".escal"))
+df.composite <- df.composite %>%
+  mutate(
+    n.c1.outcome = n.c1.outcome.reintub + n.c1.outcome.escal,
+    n.e.outcome = n.e.outcome.reintub + n.e.outcome.escal,
+    n.c1 = n.c1.reintub,
+    n.e = n.e.reintub
+  ) %>%
+  select(author, year, surgery = surgery.reintub, n.c1.outcome, n.c1, n.e.outcome, n.e)
 
 #forest 
 
@@ -237,5 +252,158 @@ m.paco2 <- metacont(n.e = n.e,
                    random = TRUE,
                    method.tau = "REML",
                    method.random.ci = "HK",
-                   title = "Early PaO2")
+                   title = "Early PaCO2")
 summary(m.paco2)
+m.ph <- metacont(n.e = n.e,
+                    mean.e = e.outcome,
+                    sd.e = e.sd,
+                    n.c = n.c1,
+                    mean.c = c1.outcome,
+                    sd.c = c1.sd,
+                    studlab = author,
+                    data = df.phearly,
+                    sm = "MD",
+                    common = TRUE,
+                    random = TRUE,
+                    method.tau = "REML",
+                    method.random.ci = "HK",
+                    title = "Early pH")
+summary(m.ph)
+summary(m.paco2)
+m.rr <- metacont(n.e = n.e,
+                 mean.e = e.outcome,
+                 sd.e = e.sd,
+                 n.c = n.c1,
+                 mean.c = c1.outcome,
+                 sd.c = c1.sd,
+                 studlab = author,
+                 data = df.rrearly,
+                 sm = "MD",
+                 common = TRUE,
+                 random = TRUE,
+                 method.tau = "REML",
+                 method.random.ci = "HK",
+                 title = "Early RR")
+summary(m.rr)
+m.sp <- metacont(n.e = n.e,
+                 mean.e = e.outcome,
+                 sd.e = e.sd,
+                 n.c = n.c1,
+                 mean.c = c1.outcome,
+                 sd.c = c1.sd,
+                 studlab = author,
+                 data = df.spoearly,
+                 sm = "MD",
+                 common = TRUE,
+                 random = TRUE,
+                 method.tau = "REML",
+                 method.random.ci = "HK",
+                 title = "Early SaO2")
+summary(m.sp)
+m.hr <- metacont(n.e = n.e,
+                 mean.e = e.outcome,
+                 sd.e = e.sd,
+                 n.c = n.c1,
+                 mean.c = c1.outcome,
+                 sd.c = c1.sd,
+                 studlab = author,
+                 data = df.hrearly,
+                 sm = "MD",
+                 common = TRUE,
+                 random = TRUE,
+                 method.tau = "REML",
+                 method.random.ci = "HK",
+                 title = "Early Heart Rate")
+summary(m.hr)
+m.map <- metacont(n.e = n.e,
+                 mean.e = e.outcome,
+                 sd.e = e.sd,
+                 n.c = n.c1,
+                 mean.c = c1.outcome,
+                 sd.c = c1.sd,
+                 studlab = author,
+                 data = df.map,
+                 sm = "MD",
+                 common = TRUE,
+                 random = TRUE,
+                 method.tau = "REML",
+                 method.random.ci = "HK",
+                 title = "Mean arterial pressure")
+summary(m.map)
+
+#sensitivity analysis
+reintu.inf <- metainf(m.reintu, pooled = "random")
+plot(reintu.inf)
+baujat(m.reintu,
+       xlim = c(0,6),
+       symbol = "slab",
+       cex = 0.8,
+       col = "blue",
+       pos = 4)
+m.reintu_sens <- update(m.reintu, 
+                         subset = !(author %in% c("Yu", "Sahin")))
+
+
+
+# 1. Create the dataframe with the exact required names
+tsa_data <- data.frame(
+  eI = c(2, 7, 1, 0, 0, 1, 0, 3, 7, 5, 2), # Intervention Events
+  nI = c(169, 108, 47, 30, 40, 90, 32, 47, 66, 51, 140), # Intervention Total
+  eC = c(0, 4, 1, 0, 2, 2, 0, 1, 2, 1, 1), # Control Events
+  nC = c(171, 112, 48, 30, 40, 90, 32, 43, 33, 49, 148) # Control Total
+)
+
+# 2. Run the RTSA
+tsa_reintu <- RTSA(
+  type    = "analysis",
+  data    = tsa_data,
+  outcome    = "OR",           # Outcome metric: Relative Risk
+  mc         = 0.8,            # Minimal clinical relevant outcome (20% RRR)
+  side       = 2,              # Two-sided hypothesis test
+  alpha      = 0.05,           # 5% Type I error
+  beta       = 0.1,            # 10% Type II error (90% power)
+  fixed      = FALSE,          # Disable fixed-effect (enables random-effects)
+  re_method  = "DL_HKSJ",      # Apply Hartung-Knapp-Sidik-Jonkman adjustment
+  random_adj = "D2",           # Adjust sample size based on Diversity (D2)
+  es_alpha   = "esOF" 
+)
+
+# 3. View the Result
+plot(tsa_reintu)
+
+#meta::funnel(m.reintu, studlab = TRUE)
+
+bias_result <- metabias(m.reintu, method.bias = "harbord")
+print(bias_result)
+funnel(m.reintu, 
+       main = "Funnel Plot: Re-intubation (k=11)",
+       xlab = "Odds Ratio (Peto Method)",
+       studlab = TRUE,    # Adds study names like Yu, Sahin, Futier
+       cex.stud = 0.8)
+
+m.reintu_highrisk <- update(m.reintu, 
+                             subgroup = highrisk, 
+                             print.subgroup.name = FALSE)
+m.reintu_rob <- update(m.reintu, 
+                            subgroup = rob, 
+                            print.subgroup.name = FALSE)
+
+m.reintu_surg <- update(m.reintu, 
+                       subgroup = surgery, 
+                       print.subgroup.name = FALSE)
+meta::forest(m.reintu_highrisk,
+             common = FALSE,                # Plot only random effects
+             random = TRUE,
+             smlab = "Reintubation Rates by Risk",
+             layout = "RevMan5",  # Plot random effects estimate
+             col.random = "black",          # Set random effects diamond color
+             digits = 2,                    # Significant digits for treatment effects
+             digits.pval = 2,               # Significant digits for p-values
+             digits.I2 = 1,                 # Significant digits for I-squared
+             digits.tau2 = 2,               # Significant digits for between-study variance
+             test.subgroup = TRUE,          # Print test for subgroup differences
+             subgroup.name = "high risk",    # Label for the grouping variable
+             sep.subgroup = "=",            # Separator between label and levels
+             print.subgroup.name = TRUE,     # Print variable name in front of labels
+             addrows.below.overall = 2,
+             )
